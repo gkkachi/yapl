f751eb3c2eeaa67a34c5ef0a22cc861ad5406fee
Konosuke Kachi <github@s.k-kachi.net>
2020-02-29 07:23:30 +0000

    add all_patterns

--- src/deck.rs
+++ src/deck.rs
@@ -6,7 +6,6 @@ pub struct Deck(Vec<Card>);
 impl Deck {
     pub fn new() -> Self {
         use itertools::Itertools;
-        use rand::{seq::SliceRandom, thread_rng};
         use Suit::*;
         use Value::*;
 
@@ -16,9 +15,30 @@ impl Deck {
         ]
         .iter();
 
-        let mut cards: Vec<_> = s.cartesian_product(v).map(|x| Card(*x.0, *x.1)).collect();
+        let cards = s.cartesian_product(v).map(|x| Card(*x.0, *x.1)).collect();
+        Deck(cards)
+    }
+
+    pub fn shuffled(self) -> Self {
+        use rand::{seq::SliceRandom, thread_rng};
+        let Deck(mut cards) = self;
         cards.shuffle(&mut thread_rng());
         Deck(cards)
+    }
+
+    pub fn remove_card(&mut self, card: &Card) {
+        let Deck(ref mut cards) = self;
+        if let Some(pos) = cards.iter().position(|x| *x == *card) {
+            cards.remove(pos);
+        }
+    }
+
+    pub fn all_patterns(self, n: usize) -> impl Iterator<Item = Vec<Card>> {
+        let m = self.0.len();
+        let Deck(cards) = self;
+        indices(m, n)
+            .into_iter()
+            .map(move |x| x.into_iter().map(|v| cards[v]).collect::<Vec<_>>())
     }
 }
 
@@ -28,5 +48,23 @@ impl IntoIterator for Deck {
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter()
+    }
+}
+
+fn indices(m: usize, n: usize) -> Vec<Vec<usize>> {
+    assert!(m >= n);
+    if n == 0 {
+        vec![vec![]]
+    } else {
+        let n = n - 1;
+        (n..m)
+            .map(|x| {
+                indices(x, n).into_iter().map(move |mut v| {
+                    v.push(x);
+                    v
+                })
+            })
+            .flatten()
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use yapl::{deck::*, hand::*};
 
 fn main() {
-    let mut d = Deck::new().into_iter();
+    let mut d = Deck::new().shuffled().into_iter();
     let x0 = d.next().unwrap();
     let x1 = d.next().unwrap();
     let y0 = d.next().unwrap();
@@ -10,21 +10,36 @@ fn main() {
     let c0 = d.next().unwrap();
     let c1 = d.next().unwrap();
     let c2 = d.next().unwrap();
-    let c3 = d.next().unwrap();
-    let c4 = d.next().unwrap();
-    let x = [x0, x1, c0, c1, c2, c3, c4];
-    let y = [y0, y1, c0, c1, c2, c3, c4];
 
-    let rank_x: HandRank = x.into();
-    let rank_y: HandRank = y.into();
+    let mut d = Deck::new();
+    [x0, x1, y0, y1, c0, c1, c2]
+        .iter()
+        .for_each(|c| d.remove_card(c));
 
-    println!("Common Cards:\n{:?}\n", [c0, c1, c2, c3, c4]);
-    println!("Player 1:\n{:?}\n{:?}\n", [x0, x1], rank_x);
-    println!("Player 2:\n{:?}\n{:?}\n", [y0, y1], rank_y);
+    let res: [_; 3] = d
+        .all_patterns(2)
+        .map(|cards| {
+            let c3 = cards[0];
+            let c4 = cards[1];
+            let x = [x0, x1, c0, c1, c2, c3, c4];
+            let y = [y0, y1, c0, c1, c2, c3, c4];
 
-    match rank_x.cmp(&rank_y) {
-        Ordering::Equal => println!("TIE"),
-        Ordering::Greater => println!("Player 1 wins."),
-        Ordering::Less => println!("Player 2 wins."),
-    };
+            let rank_x: HandRank = x.into();
+            let rank_y: HandRank = y.into();
+
+            match rank_x.cmp(&rank_y) {
+                Ordering::Equal => [0, 0, 1],
+                Ordering::Greater => [1, 0, 0],
+                Ordering::Less => [0, 1, 0],
+            }
+        })
+        .fold([0, 0, 0], |x, y| [x[0] + y[0], x[1] + y[1], x[2] + y[2]]);
+
+    println!(
+        "Community cards: {:?}\nPlayer 1: {:?}\nPlayer 2: {:?}\n{:?}",
+        [c0, c1, c2],
+        [x0, x1],
+        [y0, y1],
+        res
+    );
 }
